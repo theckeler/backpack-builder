d3856de20be1fbaae65fc0eb7d2ebc4ab70c974e
Todd Heckeler <todd@consumedesign.com>
2023-11-16 17:09:14 -0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,143 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [vanBuild, setVanBuild] = useState({
+		AccessoryRack: false,
+		vanView: {
+			Rear: false,
+			RearPassenger: true,
+			RearDriver: false,
+		},
+	});
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	useEffect(() => {
+		console.log("vanBuild:", vanBuild);
+	}, [vanBuild]);
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const radioChange = (e) => {
+		const currentRadio = e.currentTarget; // Create a copy of the vanView object
+		const updatedVanView = {}; // Set the selected radio button to true and others to false
+		Object.keys(vanBuild.vanView).forEach((key) => {
+			updatedVanView[key] = key === currentRadio.value;
+		});
+		// Update the state with the modified vanView
+		setVanBuild((prevVanBuild) => ({
+			...prevVanBuild,
+			vanView: updatedVanView,
+		}));
+	};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	return (
+		<main className="flex min-h-screen flex-col items-center justify-between relative select-none">
+			<div className="fixed z-50 right-4 top-4 bg-neutral-800 shadow p-2">
+				<ul className="">
+					<li className="grid grid-cols-[20px_1fr] gap-1 items-center group/AccessoryRack">
+						<input
+							className="appearance-none w-full h-full rounded bg-white checked:bg-red-500"
+							type="checkbox"
+							id="AccessoryRack"
+							checked={vanBuild.AccessoryRack}
+							onChange={(e) => {
+								setVanBuild({
+									...vanBuild,
+									AccessoryRack: e.currentTarget.checked,
+								});
+							}}
+						/>
+						<label
+							className="bg-yellow-500 px-4 py-2 uppercase font-bold rounded"
+							htmlFor="AccessoryRack">
+							Accessory Rack
+						</label>
+					</li>
+				</ul>
+			</div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+			<div className="fixed z-50 left-4 top-4 bg-neutral-800 shadow p-2">
+				<ul className="grid gap-2">
+					{Object.keys(vanBuild.vanView).map(function (key, i) {
+						return (
+							<li
+								className="grid grid-cols-[1fr_20px] gap-1 items-center group/Rear"
+								key={i}>
+								<label
+									className="bg-yellow-500 px-4 py-2 uppercase font-bold rounded"
+									htmlFor={key}>
+									{key}
+								</label>
+								<input
+									className="appearance-none w-full h-full rounded bg-white checked:bg-red-500"
+									type="radio"
+									id={key}
+									value={key}
+									name="vanView"
+									checked={vanBuild.vanView[key]}
+									onChange={(e) => radioChange(e)}
+								/>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+			{vanBuild.vanView.RearDriver && (
+				<div>
+					<Image
+						className="z-0 object-cover"
+						src="https://cdn.shopify.com/s/files/1/0616/3405/2352/files/sprinter-r-d.png?v=1700163309&width=2000"
+						alt=""
+						fill
+					/>
+					{vanBuild.AccessoryRack && (
+						<Image
+							className="z-10 object-cover"
+							src="https://cdn.shopify.com/s/files/1/0616/3405/2352/files/sprinter-r-d-AccessoryRack.png?v=1700164067&width=2000"
+							alt=""
+							fill
+						/>
+					)}
+				</div>
+			)}
+
+			{vanBuild.vanView.RearPassenger && (
+				<div>
+					<Image
+						className="z-0 object-cover"
+						src="https://cdn.shopify.com/s/files/1/0616/3405/2352/files/sprinter-r-p.png?v=1700163309&width=2000"
+						alt=""
+						fill
+					/>
+					{vanBuild.AccessoryRack && (
+						<Image
+							className="z-10 object-cover"
+							src="https://cdn.shopify.com/s/files/1/0616/3405/2352/files/sprinter-r-p-AccessoryRack.png?v=1700164067&width=2000"
+							alt=""
+							fill
+						/>
+					)}
+				</div>
+			)}
+
+			{vanBuild.vanView.Rear && (
+				<div>
+					<Image
+						className="z-0 object-cover"
+						src="https://cdn.shopify.com/s/files/1/0616/3405/2352/files/sprinter-r.png?v=1700163309&width=2000"
+						alt=""
+						fill
+					/>
+					{vanBuild.AccessoryRack && (
+						<Image
+							className="z-10 object-cover"
+							src="https://cdn.shopify.com/s/files/1/0616/3405/2352/files/sprinter-r-AccessoryRack.png?v=1700164067&width=2000"
+							alt=""
+							fill
+						/>
+					)}
+				</div>
+			)}
+		</main>
+	);
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "picsum.photos",
+			},
+			{
+				protocol: "https",
+				hostname: "*.shopify.com",
+			},
+		],
+		//loader: "custom",
+		//loaderFile: "./components/loader.js",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
