504891152a4ab7a9b57eab3606dfd86f16171273
Todd Heckeler <todd@consumedesign.com>
2023-11-16 18:16:07 -0500

    adding dark/light

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+
+import "@/styles/globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className="bg-white">{children}</body>
+			<body className="dark:bg-black">{children}</body>
 		</html>
 	);
 }
--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
 	const [vanBuild, setVanBuild] = useState({
+		windowMode: { dark: false, light: true },
 		AccessoryRack: false,
 		vanView: {
 			Rear: false,
@@ -14,47 +15,32 @@ export default function Home() {
 
 	useEffect(() => {
 		console.log("vanBuild:", vanBuild);
+		//localStorage.setItem("theme", vanBuild.windowMode.dark ? "dark" : "light");
+		document.querySelector("html").classList.remove("light", "dark");
+		document
+			.querySelector("html")
+			.classList.add(vanBuild.windowMode.dark ? "dark" : "light");
 	}, [vanBuild]);
 
 	const radioChange = (e) => {
+		// console.log(e.currentTarget.name);
+		const eName = e.currentTarget.name;
+
 		const currentRadio = e.currentTarget; // Create a copy of the vanView object
 		const updatedVanView = {}; // Set the selected radio button to true and others to false
-		Object.keys(vanBuild.vanView).forEach((key) => {
+		Object.keys(vanBuild[eName]).forEach((key) => {
 			updatedVanView[key] = key === currentRadio.value;
 		});
 		// Update the state with the modified vanView
 		setVanBuild((prevVanBuild) => ({
 			...prevVanBuild,
-			vanView: updatedVanView,
+			[eName]: updatedVanView,
 		}));
 	};
 
 	return (
-		<main className="flex min-h-screen flex-col items-center justify-between relative select-none">
-			<div className="fixed z-50 right-4 top-4 bg-neutral-800 shadow p-2">
-				<ul className="">
-					<li className="grid grid-cols-[20px_1fr] gap-1 items-center group/AccessoryRack">
-						<input
-							className="appearance-none w-full h-full rounded bg-white checked:bg-red-500"
-							type="checkbox"
-							id="AccessoryRack"
-							checked={vanBuild.AccessoryRack}
-							onChange={(e) => {
-								setVanBuild({
-									...vanBuild,
-									AccessoryRack: e.currentTarget.checked,
-								});
-							}}
-						/>
-						<label
-							className="bg-yellow-500 px-4 py-2 uppercase font-bold rounded"
-							htmlFor="AccessoryRack">
-							Accessory Rack
-						</label>
-					</li>
-				</ul>
-			</div>
-
+		<main
+			className={`flex min-h-screen flex-col items-center justify-between relative select-none`}>
 			<div className="fixed z-50 left-4 top-4 bg-neutral-800 shadow p-2">
 				<ul className="grid gap-2">
 					{Object.keys(vanBuild.vanView).map(function (key, i) {
@@ -79,6 +65,67 @@ export default function Home() {
 							</li>
 						);
 					})}
+				</ul>
+			</div>
+
+			<div className="fixed z-50 right-4 top-4 bg-neutral-800 shadow p-2">
+				<ul className="">
+					<li className="grid grid-cols-[20px_1fr] gap-1 items-center group/AccessoryRack">
+						<input
+							className="appearance-none w-full h-full rounded bg-white checked:bg-red-500"
+							type="checkbox"
+							id="AccessoryRack"
+							checked={vanBuild.AccessoryRack}
+							onChange={(e) => {
+								setVanBuild({
+									...vanBuild,
+									AccessoryRack: e.currentTarget.checked,
+								});
+							}}
+						/>
+						<label
+							className="bg-yellow-500 px-4 py-2 uppercase font-bold rounded"
+							htmlFor="AccessoryRack">
+							Accessory Rack
+						</label>
+					</li>
+				</ul>
+			</div>
+
+			<div className="fixed z-50 right-4 bottom-4 bg-neutral-800 shadow p-2">
+				<ul className="grid gap-2">
+					<li className="grid grid-cols-[20px_1fr] gap-1 items-center group/windowMode">
+						<input
+							className="appearance-none w-full h-full rounded bg-white checked:bg-red-500"
+							id="dark"
+							type="radio"
+							value="dark"
+							name="windowMode"
+							checked={vanBuild.windowMode.dark}
+							onChange={(e) => radioChange(e)}
+						/>
+						<label
+							className="bg-yellow-500 px-4 py-2 uppercase font-bold rounded"
+							htmlFor="dark">
+							Dark Mode
+						</label>
+					</li>
+					<li className="grid grid-cols-[20px_1fr] gap-1 items-center group/windowMode">
+						<input
+							className="appearance-none w-full h-full rounded bg-white checked:bg-red-500"
+							id="light"
+							type="radio"
+							value="light"
+							name="windowMode"
+							checked={vanBuild.windowMode.light}
+							onChange={(e) => radioChange(e)}
+						/>
+						<label
+							className="bg-yellow-500 px-4 py-2 uppercase font-bold rounded"
+							htmlFor="light">
+							Light Mode
+						</label>
+					</li>
 				</ul>
 			</div>
 
